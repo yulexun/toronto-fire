0d299438816ad4659be1f6a3758a796c0246e1e1
Yu, Lexun <lyu@prpr.one>
2024-09-16 16:19:02 -0400

    simplified

--- scripts/02-data_cleaning.r
+++ scripts/02-data_cleaning.r
@@ -66,9 +66,9 @@ selected_cleaned_fire_data <- selected_cleaned_fire_data %>%
   mutate(across(everything(), ~ na_if(
     ., "9 - Smoke alarm presence undetermined                       "
   ))) %>%
-    mutate(across(everything(), ~ na_if(
-      ., "9 - Smoke alarm presence undetermined"
-    ))) %>%
+  mutate(across(everything(), ~ na_if(
+    ., "9 - Smoke alarm presence undetermined"
+  ))) %>%
   mutate(across(everything(), ~ na_if(
     ., "9 - Activation/operation undetermined"
   )))
@@ -109,61 +109,62 @@ selected_cleaned_fire_data$time_diff <-
 
 # Simplify the presence and operation of fie alarm system,
 # smoke detector and sprinkler system
-selected_cleaned_fire_data_simple <-
+cleaned_fire_data_simple <-
   selected_cleaned_fire_data |>
   mutate(
     smoke_alarm_at_fire_origin =
       case_match(
         smoke_alarm_at_fire_origin,
-        "2 - Floor/suite of fire origin: Smoke alarm present and operated" 
-          ~ "PO",
+        "2 - Floor/suite of fire origin: Smoke alarm present and operated"
+        ~ "PO",
         "1 - Floor/suite of fire origin: No smoke alarm" ~ "N",
         "2 - Smoke alarm present and operated" ~ "PO",
         "1 - No smoke alarm" ~ "N",
-        "3 - Floor/suite of fire origin: Smoke alarm present did not operate" 
-          ~ "P",
-        "4 - Floor/suite of fire origin: Smoke alarm present, operation undetermined" ~ "P",
-        "4 - Smoke alarm present, operation undetermined" ~ "P"
+        "3 - Floor/suite of fire origin: Smoke alarm present did not operate"
+        ~ "P",
+        "4 - Floor/suite of fire origin: Smoke alarm present, operation undetermined"
+        ~ "P",
+        "4 - Smoke alarm present, operation undetermined" ~ "P",
+        "3 - Smoke alarm present did not operate" ~ "P"
       )
   )
 
-  selected_cleaned_fire_data_simple <-
-    selected_cleaned_fire_data |>
-    mutate(
-      sprinkler_system_operation =
-        case_match(
-          sprinkler_system_operation,
-          "1 - Sprinkler system activated" 
-            ~ "PO",
-          "8 - Not applicable - no sprinkler system present" ~ "N",
-          "3 - Did not activate: fire too small to trigger system" 
-            ~ "P",
-          "2 - Did not activate: remote from fire" 
-            ~ "P",
-          "5 - Did not activate: reason unknown" 
-            ~ "P",          
-          "4 - Other reason for non activation/operation"
-            ~ "P",          
-        )
-    )
-
-    selected_cleaned_fire_data_simple <-
-      selected_cleaned_fire_data |>
-      mutate(
-        fire_alarm_system_presence =
-          case_match(
-            fire_alarm_system_presence,
-            "2 - No Fire alarm system" ~ "N",
-            "1 -  Fire alarm system present" 
-              ~ "P",
-
-          )
+cleaned_fire_data_simple <-
+  cleaned_fire_data_simple |>
+  mutate(
+    sprinkler_system_operation =
+      case_match(
+        sprinkler_system_operation,
+        "1 - Sprinkler system activated"
+        ~ "PO",
+        "8 - Not applicable - no sprinkler system present" ~ "N",
+        "3 - Did not activate: fire too small to trigger system"
+        ~ "P",
+        "2 - Did not activate: remote from fire"
+        ~ "P",
+        "5 - Did not activate: reason unknown"
+        ~ "P",
+        "4 - Other reason for non activation/operation"
+        ~ "P",
       )
+  )
+
+cleaned_fire_data_simple <-
+  cleaned_fire_data_simple |>
+  mutate(
+    fire_alarm_system_presence =
+      case_match(
+        fire_alarm_system_presence,
+        "2 - No Fire alarm system" ~ "N",
+        "1 -  Fire alarm system present"
+        ~ "P",
+      )
+  )
 
 
 # Write the cleaned data to a new csv
 write_csv(
-  x = selected_cleaned_fire_data_simple,
+  x = cleaned_fire_data_simple,
   file = "data/analysis/cleaned_fire_data_simple.csv"
 )
 
--- scripts/04-test.r
+++ scripts/04-test.r
@@ -12,7 +12,7 @@ library(tidyverse)
 library(readr)
 
 #### Read data ####
-cleaned_data <- read_csv("data/analysis/cleaned_fire_data.csv", show_col_types = FALSE)
+cleaned_data <- read_csv("data/analysis/cleaned_fire_data_simple.csv", show_col_types = FALSE)
 
 #### Test data ####
 # test1: Tests that there is no empty value
@@ -23,9 +23,9 @@ print(test1)
 
 # test2: Tests that there are 70 unique area of fire hazard origin
 test2 <-
-cleaned_data$area_of_origin |>
-  unique() |>
-  length() == 70
+  cleaned_data$area_of_origin |>
+    unique() |>
+    length() == 70
 
 print(test2)
 
@@ -33,13 +33,26 @@ print(test2)
 # estimated dollar loss are non-negative
 test3 <-
   cleaned_data$civilian_casualties |> min() >= 0
-  cleaned_data$estimated_dollar_loss |> min() >= 0
+cleaned_data$estimated_dollar_loss |> min() >= 0
 
 print(test3)
 
-# test4: 
-test2 <-
-cleaned_data$fire_alarm_system_presence|>
-  unique()
+# test4, 5, 6: test if the fire alarm,
+# smoke and sprinkler system value is simplified
+test4 <-
+  cleaned_data$fire_alarm_system_presence |>
+    unique() == c("P", "N")
 
-print(test2)
+print(test4)
+
+test5 <-
+  cleaned_data$smoke_alarm_at_fire_origin |>
+    unique() == c("PO", "N", "P")
+
+print(test5)
+
+test6 <-
+  cleaned_data$sprinkler_system_operation |>
+    unique() == c("P", "N", "PO")
+
+print(test6)
