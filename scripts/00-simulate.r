3d98b562d9fde88058c7ceffe4a1446de895d458
Yu, Lexun <lyu@prpr.one>
2024-09-16 23:32:35 -0400

    change simulation style

--- scripts/00-simulate.r
+++ scripts/00-simulate.r
@@ -16,35 +16,42 @@ n <- 100
 
 # Generate random alarm times (dates within the last 5 years)
 set.seed(123)
-alarm_times <- sample(seq(ymd_hms('2018-01-01 00:00:00'), 
-                          ymd_hms('2023-01-01 00:00:00'), 
-                          by="min"), n, replace=TRUE)
+alarm_times <- sample(seq(ymd_hms("2018-01-01 00:00:00"),
+  ymd_hms("2023-01-01 00:00:00"),
+  by = "min"
+), n, replace = TRUE)
 
 # Generate random time differences for arrival times
-time_diff <- runif(n, min=2, max=10)
+time_diff <- runif(n, min = 2, max = 10)
 
 # Calculate arrival times based on alarm times and time difference
 arrival_times <- alarm_times + time_diff
 
 # Simulate categorical columns
-area_of_origin <- sample(c("24 - Cooking Area or Kitchen", 
-                           "12 - Hallway, Corridor", 
-                           "79 - Other Outside Area"), n, replace=TRUE)
+area_of_origin <- sample(c(
+  "24 - Cooking Area or Kitchen",
+  "12 - Hallway, Corridor",
+  "79 - Other Outside Area"
+), n, replace = TRUE)
 
-extent_of_fire <- sample(c("1 - Confined to object of origin", 
-                           "2 - Confined to part of room/area of origin"), n, replace=TRUE)
+extent_of_fire <- sample(c(
+  "1 - Confined to object of origin",
+  "2 - Confined to part of room/area of origin"
+), n, replace = TRUE)
 
-ignition_source <- sample(c("41 - Other Heating Equipment", 
-                            "11 - Stove, Range-top burner", 
-                            "24 - Circuit Wiring - Copper"), n, replace=TRUE)
+ignition_source <- sample(c(
+  "41 - Other Heating Equipment",
+  "11 - Stove, Range-top burner",
+  "24 - Circuit Wiring - Copper"
+), n, replace = TRUE)
 
-fire_alarm_system_presence <- sample(c("P", "N"), n, replace=TRUE)
-smoke_alarm_at_fire_origin <- sample(c("PO", "N", "p"), n, replace=TRUE)
-sprinkler_system_operation <- sample(c("P", "N", "PO"), n, replace=TRUE)
+fire_alarm_system_presence <- sample(c("P", "N"), n, replace = TRUE)
+smoke_alarm_at_fire_origin <- sample(c("PO", "N", "p"), n, replace = TRUE)
+sprinkler_system_operation <- sample(c("P", "N", "PO"), n, replace = TRUE)
 
 # Simulate numerical columns
-civilian_casualties <- sample(0:5, n, replace=TRUE)
-estimated_dollar_loss <- sample(0:60000, n, replace=TRUE)
+civilian_casualties <- sample(0:5, n, replace = TRUE)
+estimated_dollar_loss <- sample(0:60000, n, replace = TRUE)
 
 # Create the simulated dataframe
 simulated_data <- data.frame(
